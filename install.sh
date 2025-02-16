db0daed370b9afea605dfc211c51260d5c3efc79
f22hakuti <f22hakuti@gmail.com>
2025-02-16 23:16:05 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 echo "╔══╣ Install: easyocr_ros (STARTING) ╠══╗"
 
+cd ..
+git clone -b feature/humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+
 sudo apt update
 pip install easyocr pyyaml
 source ~/.bashrc
