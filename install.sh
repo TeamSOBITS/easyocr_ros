20dc547fcbdfdc8453adb609cc77192fceac39ec
f22hakuti <131255788+f22hakuti@users.noreply.github.com>
2025-07-17 14:39:35 +0900

    Update install branch name of sobits_msgs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "╔══╣ Install: easyocr_ros (STARTING) ╠══╗"
 
 cd ..
-git clone -b feature/humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+git clone -b humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
 
 sudo apt update
 pip install easyocr pyyaml
